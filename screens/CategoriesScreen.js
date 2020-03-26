603e0c7b16019e8f7ea020000731a15dc1983b94
julien legay <legayj@gmail.com>
2020-03-26 17:14:58 +0100

    Add data & passing data from one screen to another"

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,17 +1,56 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import {
+	View,
+	Text,
+	FlatList,
+	StyleSheet,
+	TouchableOpacity,
+	Platform
+} from "react-native";
+
+import Colors from "../constants/Colors";
+
+import { CATEGORIES } from "../data/dummy-data";
 
 const CategoriesScreen = props => {
-	const navigateToCategory = () => {
-		props.navigation.navigate("CategoryMeal");
+	const navigateToCategory = ({ item }) => {
+		props.navigation.navigate("CategoryMeal", {
+			categoryId: item.id,
+			color: item.color,
+			title: item.title
+		});
+	};
+
+	const renderGridItem = itemData => {
+		return (
+			<TouchableOpacity
+				style={styles.gridItem}
+				onPress={() => navigateToCategory(itemData)}
+				itemData={itemData}
+			>
+				<View>
+					<Text>{itemData.item.title}</Text>
+				</View>
+			</TouchableOpacity>
+		);
 	};
 
 	return (
-		<View style={styles.screen}>
-			<Text>The Categories Screen!</Text>
-			<Button title="Go to Meals!" onPress={() => navigateToCategory()} />
-		</View>
+		<FlatList
+			keyExtractor={(item, index) => item.id} // not mandatory (id is now accepted as key)
+			data={CATEGORIES}
+			renderItem={renderGridItem}
+			numColumns={2}
+		/>
 	);
+};
+
+CategoriesScreen.navigationOptions = {
+	headerTitle: "Meal Categories",
+	headerStyle: {
+		backgroundColor: Platform.OS === "android" ? Colors.primary : ""
+	},
+	headerTintColor: Platform.OS === "android" ? "white" : Colors.primary
 };
 
 const styles = StyleSheet.create({
@@ -19,6 +58,11 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center"
+	},
+	gridItem: {
+		flex: 1,
+		margin: 15,
+		height: 120
 	}
 });
 
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,20 +1,38 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 
+import { CATEGORIES } from "../data/dummy-data";
+
 const CategoryMealsScreen = props => {
+	const catId = props.navigation.getParam("categoryId");
+	const selectedCategory = CATEGORIES.find(c => c.id === catId);
+
 	const navigateToMealDetail = () => {
 		props.navigation.navigate("MealDetail");
 	};
 
 	return (
 		<View style={styles.screen}>
-			<Text>The Category Meals Screen!</Text>
-			<Button
-				title="Go to Meals!"
-				onPress={() => navigateToMealDetail()}
-			/>
+			<Text>{selectedCategory.title}</Text>
+			<Button title="Go to Details!" onPress={navigateToMealDetail} />
+			<Button title="Go Back" onPress={() => props.navigation.pop()} />
 		</View>
 	);
+};
+
+CategoryMealsScreen.navigationOptions = navigationData => {
+	const catId = navigationData.navigation.getParam("categoryId");
+	const selectedCategory = CATEGORIES.find(c => c.id === catId);
+
+	return {
+		headerTitle: selectedCategory.title,
+		headerStyle: {
+			backgroundColor:
+				Platform.OS === "android" ? selectedCategory.color : ""
+		},
+		headerTintColor:
+			Platform.OS === "android" ? "white" : selectedCategory.color
+	};
 };
 
 const styles = StyleSheet.create({
