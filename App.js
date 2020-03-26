93409d832b20120292a10dd215a5189048c5ac3f
julien legay <legayj@gmail.com>
2020-03-26 12:26:21 +0100

    add some fonts

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const fetchFonts = () => {
+	return Font.loadAsync({
+		"open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+		"open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
+	});
+};
 
 export default function App() {
-	return <View style={styles.container}></View>;
+	const [fontLoaded, setfontLoaded] = useState(false);
+
+	if (!fontLoaded) {
+		return (
+			<AppLoading
+				startAsync={fetchFonts}
+				onFinish={() => setfontLoaded(true)}
+			/>
+		);
+	}
+
+	return <View></View>;
 }
